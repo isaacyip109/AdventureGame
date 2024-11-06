8e4fb42d5773b87631628763831e10241cd55a95
isaacyip109 <tsyip@smail.pembinatrails.ca>
2024-11-06 14:13:11 -0600

    Initial Commit

--- game.sh
+++ game.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 echo "Welcome to the Adventure Game!"
+echo "Choose a location to explore: forest, castle, or cave."
+read locaion
+if [ "location" == "forest" ]; then
+  cat castle.txt
+elif [ "$location" == "castle" ]; then
+  cat castle.txt
+elif [ "$location" == "cave" ]; then
+  cat cave.txt
+  echo "But wait..."
+  cat monster.txt
+else
+  echo "Invalid location"
+  
